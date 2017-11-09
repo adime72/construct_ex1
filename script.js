4cd931246eb56fdac8366737567895ae2e24a495
Adi Meirovitch <adime.support@gmail.com>
2017-11-09 13:17:12 +0200

    building a new array

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-
+    var cars = [];
 
     function Car(brand, modal, year, km) {
         this.newBrand = brand;
@@ -9,7 +9,7 @@ $(document).ready(function () {
         //this.fullName = this.firstName + " " + this.lastName;
         this.errCar = function () { return this.firstName + " " + this.lastName };
 
-            alert("Submitted");
+        alert("Submitted");
     };
 
 
@@ -18,17 +18,28 @@ $(document).ready(function () {
     $("#btn-add").click(function () {
         // Read all data
         var brand = document.getElementById("brand").value;
-        var modal = document.getElementById("model").value;
+        var model = document.getElementById("model").value;
         var year = document.getElementById("year").value;
         var km = document.getElementById("km").value;
+        cars = [brand, model, year, km]
+    
 
 
         // Create car instance using the data above
+
+
+/*
+        // This creates an instance of the object Person
+        var nir = new Person("Nir", "Channes");
+        var john = new Person("John", "Doe");
+        console.log(person);
+        console.log(nir);
+        console.log(nir.fullName());
         // var car = new Car(...)
 
         // Add car to array
         // arrCar.push(car);
-
+*/
 
     });
 });
