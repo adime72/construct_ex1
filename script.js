2068e9cb2a54543c4f09f77ee126859ef90ee294
Adi Meirovitch <adime.support@gmail.com>
2017-11-09 12:46:38 +0200

    first array get the input

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
-$(document).ready(function() { 
-    $("#btnCalc").click(function() { 
-    
+$(document).ready(function () {
 
-    }
-}
+
+    function Car(brand, modal, year, km) {
+        this.newBrand = brand;
+        this.newModel = model;
+        this.newYear = year;
+        this.newKm = km;
+        //this.fullName = this.firstName + " " + this.lastName;
+        this.errCar = function () { return this.firstName + " " + this.lastName };
+
+            alert("Submitted");
+    };
+
+
+
+
+    $("#btn-add").click(function () {
+        // Read all data
+        var brand = document.getElementById("brand").value;
+        var modal = document.getElementById("model").value;
+        var year = document.getElementById("year").value;
+        var km = document.getElementById("km").value;
+
+
+        // Create car instance using the data above
+        // var car = new Car(...)
+
+        // Add car to array
+        // arrCar.push(car);
+
+
+    });
+});
+
